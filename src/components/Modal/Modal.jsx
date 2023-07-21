1984d8097cac5bc62f833d8feb08ef4bf7e7f79e
EvgenBatt <evgenmgid@gmail.com>
2023-07-21 17:57:34 +0300

    Update Task

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -30,43 +30,6 @@ export const Modal = ({ closeModal, bigImageUrl, imageTags }) => {
   );
 };
 
-// export class Modal extends Component {
-//   handleEsc = e => {
-//     if (e.code === 'Escape') this.props.closeModal();
-//   };
-
-//   componentDidMount() {
-//     document.addEventListener('keydown', this.handleEsc);
-//   }
-
-//   componentWillUnmount() {
-//     document.removeEventListener('keydown', this.handleEsc);
-//   }
-
-//   clickBackdrop = e => {
-//     if (e.target === e.currentTarget) {
-//       this.props.closeModal();
-//     }
-//   };
-
-//   render() {
-//     const { bigImageUrl, imageTags } = this.props;
-//     return (
-//       <Overlay onClick={this.clickBackdrop}>
-//         <ModalDiv>
-//           <Button onClick={this.clickBackdrop}>X</Button>
-//           <img
-//             src={bigImageUrl}
-//             alt={imageTags}
-//             loading="lazy"
-//             width="1000px"
-//           />
-//         </ModalDiv>
-//       </Overlay>
-//     );
-//   }
-// }
-
 Modal.propTypes = {
   bigImageUrl: PropTypes.string.isRequired,
   imageTags: PropTypes.string.isRequired,
